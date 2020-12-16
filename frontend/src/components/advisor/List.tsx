19c4d67a3ec87f659cff3c2c324d2072c33bb4f1
Adrianna Juchacz <adrianna.juchacz@gmail.com>
2020-12-16 22:49:27 +0100

    added a list of namespaces in the Network Advisor

--- frontend/src/components/advisor/List.tsx
+++ frontend/src/components/advisor/List.tsx
@@ -5,13 +5,55 @@ import Link from '../common/Link';
 import { SectionBox } from '../common/SectionBox';
 import SectionFilterHeader from '../common/SectionFilterHeader';
 import SimpleTable from '../common/SimpleTable';
+import Namespace from '../../lib/k8s/namespace';
+import { StatusLabel } from '../common/Label';
 
-export default function AdvisorList(){
-    const [advisor, error] = Advisor.useList();
+
+export default function AdvisorList() {
+    const [namespaces, error] = Namespace.useList();
+    const filterFunc = useFilterFunc();
+
+    function makeStatusLabel(namespace: Namespace) {
+        const status = namespace.status.phase;
+        return (
+            <StatusLabel status={status === 'Active' ? 'success' : 'error'}>
+                {status}
+            </StatusLabel>
+        );
+    }
 
     return (
         <SectionBox
-            title="Network Policy Advisor"
-        />
+
+            title={
+                <SectionFilterHeader
+                    title="Network Policy Advisor"
+                    noNamespaceFilter
+                    headerStyle="main"
+                />
+            }
+        >
+            <SimpleTable
+                rowsPerPage={[100]}
+                filterFunction={filterFunc}
+                errorMessage={Namespace.getErrorMessage(error)}
+                columns={[
+                    {
+                        label: 'Name',
+                        getter: (namespace) => namespace.getName(),
+                        sort: (n1: Namespace, n2: Namespace) => {
+                            if (n1.metadata.name < n2.metadata.name) {
+                                return -1;
+                            } else if (n1.metadata.name > n2.metadata.name) {
+                                return 1;
+                            }
+                            return 0;
+                        }
+                    },
+                ]}
+                data={namespaces}
+                defaultSortingColumn={3}
+            />
+        </SectionBox>
     )
 }
