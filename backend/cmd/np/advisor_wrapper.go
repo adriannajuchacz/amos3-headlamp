78c738eed32d57bb386a6bb076d3c0c1f9ed7470
witzki <philipp.wisznawitzki@campus.tu-berlin.de>
2021-01-22 10:04:51 +0100

    rework wrappper start stop wrapper over HTTP call

--- backend/cmd/np/advisor_wrapper.go
+++ backend/cmd/np/advisor_wrapper.go
@@ -1,31 +1,58 @@
 package networkPolicy
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"log"
 	"os"
 	"os/exec"
 	"strings"
-	"sync"
-//	"bufio"
 )
 
-// var (
-// 	running bool = false
-// )
-func Report (namespace string) {
+var (
+	running bool = false
+)
+
+func Report(namespace string) {
 	log.Println("####### Sending a Network Policy Report for:", namespace)
+
+	file, err := os.Open("networktrace.log")
+	if err != nil {
+		log.Println("Error opening networktrace.log: %s", err)
+	}
+
+	fileScanner := bufio.NewScanner(file)
+	var text []string
+
+	for fileScanner.Scan() {
+		text = append(text, fileScanner.Text())
+	}
+
+	file.Close()
+
+	i := 0
+	for i < len(text)-1 {
+		i += 1
+		log.Println(text[i])
+	}
 }
 
-func Start (namespace string) {
+func Start(namespace string) {
 	log.Println("####### Starting the Network Policy for:", namespace)
+	var command = join("monitor --namespaces ", namespace)
+	command = join(command, " --output ./networktrace.log")
+	running = true
+	go advisorRealTimeWrapper(command)
+
 }
 
-func Stop (namespace string) {
+func Stop(namespace string) {
 	log.Println("####### Stoping the Network Policy for:", namespace)
+	running = false
+	generateReport("networktrace.log", "network-policy.yaml")
+	os.Remove("networktrace.log")
 }
-
 
 func copyAndCapture(w io.Writer, r io.Reader) ([]byte, error) {
 	var out []byte
@@ -65,7 +92,6 @@ func advisorRealTimeWrapper(args ...string) {
 	var advisorString = "kubectl gadget network-policy "
 	var command string
 
-	// args length control
 	if len(args) == 0 {
 		command = advisorString
 	} else if len(args) == 1 {
@@ -78,77 +104,31 @@ func advisorRealTimeWrapper(args ...string) {
 		command = join(advisorString, sb.String())
 	}
 
-	// start advisor and print on console
 	cmd := exec.Command("/bin/sh", "-c", command)
 
-	var stdout, stderr []byte
-	var errStdout, errStderr error
+	var stdout []byte
+	var errStdout error
 	stdoutIn, _ := cmd.StdoutPipe()
-	stderrIn, _ := cmd.StderrPipe()
+
 	err := cmd.Start()
 	if err != nil {
 		log.Fatalf("cmd.Start() failed with '%s'\n", err)
 	}
 
-	// cmd.Wait() should be called only after we finish reading
-	// from stdoutIn and stderrIn.
-	// wg ensures that we finish
-	var wg sync.WaitGroup
-	wg.Add(1)
 	go func() {
 		stdout, errStdout = copyAndCapture(os.Stdout, stdoutIn)
-		wg.Done()
 	}()
 
-	stderr, errStderr = copyAndCapture(os.Stderr, stderrIn)
+	for {
+		if running != true {
 
-	wg.Wait()
-
-	err = cmd.Wait()
-	if err != nil {
-		log.Fatalf("cmd.Run() failed with %s\n", err)
+			if err := cmd.Process.Kill(); err != nil {
+				fmt.Println("failed to kill process: ", err)
+			}
+			break
+		}
 	}
-	if errStdout != nil || errStderr != nil {
-		log.Fatal("failed to capture stdout or stderr\n")
-	}
-	outStr, errStr := string(stdout), string(stderr)
-	fmt.Printf("\nout:\n%s\nerr:\n%s\n", outStr, errStr)
 }
-
-// func startAdvisorMonitor(namespace string, outputfile string) {
-// 	if running {
-// 		fmt.Print("Allready monitoring")
-// 		return
-// 	} else {
-// 		fmt.Printf("Start Monitoring on '%s' in file '%s' \n", namespace, outputfile)
-// 		running = true
-
-// 		var advisorString = "kubectl gadget network-policy monitor --namespace "
-// 		var command string
-
-// 		command = join(advisorString, namespace)
-// 		command = join(command, " --output ./")
-// 		command = join(command, outputfile)
-
-// 		fmt.Printf(command)
-// 		fmt.Printf("\n")
-
-// 		cmd := exec.Command("/bin/sh", "-c", command)
-// 		cmd.Start()
-// 		cmd.Wait()
-
-// 		for {
-// 			if running == false {
-// 				fmt.Printf("Ich beende \n")
-// 				return
-// 			} else {
-// 				fmt.Print(running)
-// 				time.Sleep(time.Second)
-// 			}
-// 		}
-// 	}
-
-// }
 
 func generateReport(inputfile string, outputfile string) {
 	var command = "kubectl gadget network-policy report --input ./"
@@ -163,15 +143,28 @@ func generateReport(inputfile string, outputfile string) {
 
 // For Testing pls uncomment the func main()
 // it is for bulding commented
-/* 
-func main() {
- 	// go startAdvisorMonitor("demo", "networktrace.log")
 
- 	go advisorRealTimeWrapper("monitor --namespaces demo --output ./networktrace.log")
+// func main() {
+// 	var namespace = "demo"
+// 	var befehl = join("monitor --namespaces ", namespace)
+// 	befehl = join(befehl, " --output ./networktrace.log")
+// 	fmt.Println(befehl)
 
- 	input := bufio.NewScanner(os.Stdin)
- 	input.Scan()
+// 	running = true
+// 	go advisorRealTimeWrapper(befehl)
 
- 	generateReport("networktrace.log", "network-policy.yaml")
- }
- */
+// 	input := bufio.NewScanner(os.Stdin)
+// 	input.Scan()
+
+// 	fmt.Println("wir schließ0en den shit")
+// 	// time.Sleep(10 * time.Second)
+// 	// fmt.Println("stop running")
+// 	running = false
+
+// 	// time.Sleep(2 * time.Second)
+
+// 	fmt.Println("geschlossen")
+
+// 	generateReport("networktrace.log", "network-policy.yaml")
+// 	// os.Remove("networktrace.log")
+// }
